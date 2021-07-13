92103df5bafada53f029807613cc7b59811cbba4
cainjiang <cainjiang@tencent.com>
2021-07-13 15:39:41 +0800

    feat(texture): 倒腾纹理相关知识

--- app/src/main/res/raw/egl_texture_fragtment_shader.glsl
+++ app/src/main/res/raw/egl_texture_fragtment_shader.glsl
@@ -3,10 +3,9 @@ precision mediump float;
 // 从上一个shader中获取输入
 in vec2 v_texCoord;
 out vec4 out_Color;
-// 纹理单元(采样器)
+// 采样器
 uniform sampler2D s_texture;
 
 void main(){
     out_Color = texture(s_texture, v_texCoord);
-//    gl_FragColor = texture(s_texture, v_texCoord);
 }
